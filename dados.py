82267de798e67a338c05e0b572837034764b9bee
Enricco935 <enriccofaria2018@gmail.com>
2026-06-25 18:21:11 -0300

    pra nao travar dnv

--- dados.py
+++ dados.py
@@ -1,37 +1,33 @@
+from data_base import *
 class Chapa:
-    def __init__(self, id, tipo, tempo_processamento, setup):
+    def __init__(self, id, tipo, tempo_processamento):
         self.id = id
         self.tipo = tipo
-
         self.tempo_processamento = tempo_processamento
-        self.setup = setup
-
 class Pedido:
-    def __init__(self, chapas, tempo_processamento, data_termino, custo_antecipacao, custo_atraso, setup, ):
+    def __init__(self, id, chapas, tempo_processamento, data_termino, custo_antecipacao, custo_atraso):
+        self.id = id
         self.chapas = chapas # chapas do pedido
         self.tempo_processamento = tempo_processamento 
         self.data_termino = data_termino
         self.custo_antecipacao = custo_antecipacao
         self.custo_atraso = custo_atraso
-        self.setup = setup #tupla setup em relacao a outros pedidos
 class Job: 
     # informacoes necessarias para cada job
     def __init__(self, tempo_processamento, data_termino, setup, custo_antecipacao, custo_atraso):
         self.tempo_processamento = tempo_processamento
         self.data_termino = data_termino
-        self.setup = setup #tupla setup em relacao a outros jobs
         self.custo_antecipacao = custo_antecipacao
         self.custo_atraso = custo_atraso
 class Entrada_Modelo:
     def __init__(self, n, P, D, S, a, b):
         self.n = n #numero de jobs
-        self.P = P #tupla tempo processamento dos jobs
-        self.D = D #tupla data desejada para o termino
-        self.S = S #matriz Setup
+        self.P = P #vetor tempo processamento dos jobs
+        self.D = D #vetor data desejada para o termino
+        self.S = S #dict Setup
         self.a = a #tupla custo antecipacao dos jobs
         self.b = b #tupla custo atraso dos jobs
         self.M = 99999
-
 class Saida_Modelo:
     def __init__(self, s, y, e, t, Z):
         self.s = s #tupla tempo inicio do processamento
@@ -39,3 +35,60 @@ class Saida_Modelo:
         self.e = e #tupla tempo antecipacao
         self.t = t #tupla tempo de atraso
         self.Z = Z #custo
+
+
+def dicionario_setup(dados_setup):
+    setups = {}
+    for origem, destino, tempo in dados_setup:
+        setups[(origem, destino)] = tempo
+    return setups
+
+def chapas_por_pedido(pedido_id):
+    chapas = []
+    dados_chapas_pedido = listar_chapas_por_pedido(pedido_id)
+    for id, tipo, tempo_processamento_chapa, quantidade in dados_chapas_pedido:
+        chapas.append(Chapa(id, tipo, tempo_processamento * quantidade))
+
+    return chapas
+
+def dados_entrada_modelo(jobs, dados_setup):
+    n_jobs = len(jobs)
+
+    vetor_tempo_processamento = []
+    vetor_data_termino = []
+    vetor_custo_antecipacao = []
+    vetor_custo_atraso = []
+    setups = dicionario_setup(dados_setup)
+
+    for job in jobs:
+        vetor_tempo_processamento.append(job.tempo_processamento)
+        vetor_data_termino.append(job.data_termino)
+        vetor_custo_antecipacao.append(job.custo_antecipacao)
+        vetor_custo_atraso.append(job.custo_atraso)
+
+    return Entrada_Modelo(n_jobs, vetor_tempo_processamento, vetor_data_termino, setups, vetor_custo_antecipacao, vetor_custo_atraso)
+    
+
+dados_chapas = listar_chapas()
+dados_setups = listar_setups()
+dados_pedido = listar_pedidos()
+
+chapas = []
+pedidos = []
+jobs = []
+setups = dicionario_setup(dados_setups)
+entrada = None
+
+#===== preenche chapas
+for i, (id_chapa, tipo, tempo_processamento) in enumerate(dados_chapas):
+    chapas.append(Chapa(id_chapa, tipo, tempo_processamento))
+
+for id, data_termino, custo_antecipacao, custo_atraso, tempo_processamento, chapas_distintas, qtd_chapas in dados_pedido:
+    pedidos.append(Pedido(id, chapas_por_pedido(id), tempo_processamento, data_termino, custo_antecipacao, custo_atraso))
+
+#tempo de setup entre uma chapa e outra e representado por dicionario
+for pedido in pedidos:
+    for chapa in pedido.chapas:
+        jobs.append(Job(chapa.tempo_processamento, pedido.data_termino, pedido.custo_antecipacao, pedido.custo_atraso))
+
+entrada = dados_entrada_modelo(jobs, dados_setups)
--- modelo.py
+++ modelo.py
@@ -1,3 +1,34 @@
+from pulp import *
+import dados as dt
 
+
+
+
+# Cria o problema
+prob = LpProblem("Sequenciamento Maquina", LpMinimize)
+
+# Variáveis
+variaveis = dt.Entrada_Modelo()
+
+for i,job in enumerate(dt.jobs): 
+    x = LpVariable(f"x{i}", lowBound=0)
+    y = LpVariable(f"y{i}", lowBound=0)
+
+# Função objetivo
+prob += 3*x + 2*y
+
+# Restrições
+prob += x + y <= 4
+prob += x <= 2
+prob += y <= 3
+
+# Resolve
+prob.solve()
+
+print("Status:", LpStatus[prob.status])
+
+print("x =", value(x))
+print("y =", value(y))
+print("Lucro =", value(prob.objective))
 
 #Quando nois pegar pra fazer esse trem junto...
